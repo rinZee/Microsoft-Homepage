6a969e845b2c59d949f3b165e729f7d62b21e6b8
Tsheten Renzee Sherpa <trex33160@gmail.com>
2020-09-05 14:00:03 +0545

    Updates

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const slides = document.querySelectorAll(".slide");
 const next = document.querySelector("#next");
 const prev = document.querySelector("#prev");
-const auto = true;
+const auto = false;
 const intervalTime = 5000;
 let slideInterval;
 
