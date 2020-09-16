57f37cdeb741050212c8ac62d63426e21b377de4
Tsheten Renzee Sherpa <trex33160@gmail.com>
2020-09-16 16:29:06 +0545

    added menu button for mobile devices

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,5 @@
-
+const button = document.querySelector(".menu-btn");
+const menu = document.querySelector(".main-menu");
+button.addEventListener("click", () => {
+  menu.classList.toggle("show");
+});
